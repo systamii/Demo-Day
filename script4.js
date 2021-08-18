fa9b4ea65613412fa32c5f9d4ee9c83bdce98c5d
John Chandler <jchandleriii40@stuy.edu>
2021-08-18 09:48:52 -0400

    eighth commit

--- script4.js
+++ script4.js
@@ -1,22 +1,89 @@
-let Ichigo = document.createElement("div")
-let IchigoImage = document.createElement('img');
-IchigoImage.src = "Ichigo.jpg";
-let IchigoAnchor = document.createElement("a");
-IchigoAnchor.href = "index2.html";
-let IchigoName = document.createElement("p");
-let products = document.querySelector(".displayProduct");
-let IchigoPrice = document.createElement("p");
-IchigoName.innerHTML = "BLEACH: ICHIGO KUROSAKI 1/6 SCALE PREMIUM STATUE";
-IchigoPrice.innerHTML = "$699";
-IchigoName.id = "IchigoName";
-IchigoPrice.id = "IchigoPrice"
-products.appendChild(Ichigo);
-Ichigo.appendChild(IchigoAnchor);
-IchigoAnchor.appendChild(IchigoImage);
 
+let fma = document.createElement("div")
+let fmaImage = document.createElement('img');
+fmaImage.src = "FMA.jpg";
+let fmaAnchor = document.createElement("a");
+fmaAnchor.href = "index2.html";
+let fmaName = document.createElement("p");
+let fmaPrice = document.createElement("p");
+fmaName.innerHTML = "Edward Elric – A Fierce Counter Attack! 1/6 Scale Premium Statue (LIMITED)"
+fmaPrice.innerHTML = "$899";
+fmaName.id = "IchigoName";
+fmaPrice.id = "IchigoPrice";
+let products = document.querySelector(".displayCollectibles");
+products.appendChild(fma);
+fma.appendChild(fmaAnchor);
+fmaAnchor.appendChild(fmaImage);
+fma.appendChild(fmaName);
+fma.appendChild(fmaPrice);
 
-Ichigo.appendChild(IchigoName);
-Ichigo.appendChild(IchigoPrice);
+let enel = document.createElement("div")
+let enelImage = document.createElement('img');
+enelImage.src = "enel.jpg";
+let enelAnchor = document.createElement("a");
+enelAnchor.href = "index2.html";
+let enelName = document.createElement("p");
+let enelPrice = document.createElement("p");
+enelName.innerHTML = "One Piece - BP Studio - OP03 God Enel 1/6 Premium Resin Statue";
+enelPrice.innerHTML = "$699";
+enelName.id = "EnelName";
+enelPrice.id = "EnelPrice"
+products.appendChild(enel);
+enel.appendChild(enelAnchor);
+enelAnchor.appendChild(enelImage);
+enel.appendChild(enelName);
+enel.appendChild(enelPrice);
+
+let byakuya = document.createElement("div")
+let byakuyaImage = document.createElement('img');
+byakuyaImage.src = "byakuya.jpg";
+let byakuyaAnchor = document.createElement("a");
+byakuyaAnchor.href = "index2.html";
+let byakuyaName = document.createElement("p");
+let byakuyaPrice = document.createElement("p");
+byakuyaName.innerHTML = "Kuchiki Byakuya – Bleach Resin Statue – T.P.A Q Tan JT Studios";
+byakuyaPrice.innerHTML = "$89";
+byakuyaName.id = "IchigoName";
+byakuyaPrice.id = "IchigoPrice"
+products.appendChild(byakuya);
+byakuya.appendChild(byakuyaAnchor);
+byakuyaAnchor.appendChild(byakuyaImage);
+byakuya.appendChild(byakuyaName);
+byakuya.appendChild(byakuyaPrice);
+
+let dio = document.createElement("div")
+let dioImage = document.createElement('img');
+dioImage.src = "dio.jpg";
+let dioAnchor = document.createElement("a");
+dioAnchor.href = "index2.html";
+let dioName = document.createElement("p");
+let dioPrice = document.createElement("p");
+dioName.innerHTML = "Dio Brando Statue Resin JOS STUDIO JOJO'S BIZARRE ADVENTURE 1/6";
+dioPrice.innerHTML = "$249";
+dioName.id = "IchigoName";
+dioPrice.id = "IchigoPrice"
+products.appendChild(dio);
+dio.appendChild(dioAnchor);
+dioAnchor.appendChild(dioImage);
+dio.appendChild(dioName);
+dio.appendChild(dioPrice);
+
+let Vasto = document.createElement("div")
+let VastoImage = document.createElement('img');
+VastoImage.src = "Vasto.jpg";
+let VastoAnchor = document.createElement("a");
+VastoAnchor.href = "index2.html";
+let VastoName = document.createElement("p");
+let VastoPrice = document.createElement("p");
+VastoName.innerHTML = "Ichigo VS Ulqiorra; Elite Fandom Statue";
+VastoPrice.innerHTML = "$699";
+VastoName.id = "IchigoName";
+VastoPrice.id = "IchigoPrice"
+products.appendChild(Vasto);
+Vasto.appendChild(VastoAnchor);
+VastoAnchor.appendChild(VastoImage);
+Vasto.appendChild(VastoName);
+Vasto.appendChild(VastoPrice);
 
 let DBZ = document.createElement("div")
 DBZ.href = "index3.html";
@@ -35,6 +102,8 @@ GokuAnchor.appendChild(GokuImage);
 DBZ.appendChild(GokuAnchor);
 DBZ.appendChild(GokuName);
 DBZ.appendChild(GokuPrice);
+
+
 let Shinra = document.createElement("div")
 let shinraimg = document.createElement("img")
 shinraimg.src = "shinra.jpg";
