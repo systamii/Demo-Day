2dba9f25b41e22c445650aaf7fd65f4139e5a3f5
John Chandler <jchandleriii40@stuy.edu>
2021-08-18 10:19:33 -0400

    tenth commit

--- script4.js
+++ script4.js
@@ -3,7 +3,7 @@ let fma = document.createElement("div")
 let fmaImage = document.createElement('img');
 fmaImage.src = "FMA.jpg";
 let fmaAnchor = document.createElement("a");
-fmaAnchor.href = "index2.html";
+fmaAnchor.href = "elric.html";
 let fmaName = document.createElement("p");
 let fmaPrice = document.createElement("p");
 fmaName.innerHTML = "Edward Elric – A Fierce Counter Attack! 1/6 Scale Premium Statue (LIMITED)"
@@ -21,28 +21,29 @@ let enel = document.createElement("div")
 let enelImage = document.createElement('img');
 enelImage.src = "enel.jpg";
 let enelAnchor = document.createElement("a");
-enelAnchor.href = "index2.html";
+enelAnchor.href = "enel.html";
 let enelName = document.createElement("p");
 let enelPrice = document.createElement("p");
 enelName.innerHTML = "One Piece - BP Studio - OP03 God Enel 1/6 Premium Resin Statue";
-enelPrice.innerHTML = "$699";
-enelName.id = "EnelName";
-enelPrice.id = "EnelPrice"
+enelPrice.innerHTML = "$1,699";
+enelName.id = "IchigoName";
+enelPrice.id = "IchigoPrice"
 products.appendChild(enel);
 enel.appendChild(enelAnchor);
 enelAnchor.appendChild(enelImage);
 enel.appendChild(enelName);
 enel.appendChild(enelPrice);
 
+
 let byakuya = document.createElement("div")
 let byakuyaImage = document.createElement('img');
 byakuyaImage.src = "byakuya.jpg";
 let byakuyaAnchor = document.createElement("a");
-byakuyaAnchor.href = "index2.html";
+byakuyaAnchor.href = "byakuya.html";
 let byakuyaName = document.createElement("p");
 let byakuyaPrice = document.createElement("p");
 byakuyaName.innerHTML = "Kuchiki Byakuya – Bleach Resin Statue – T.P.A Q Tan JT Studios";
-byakuyaPrice.innerHTML = "$89";
+byakuyaPrice.innerHTML = "$899";
 byakuyaName.id = "IchigoName";
 byakuyaPrice.id = "IchigoPrice"
 products.appendChild(byakuya);
@@ -55,7 +56,7 @@ let dio = document.createElement("div")
 let dioImage = document.createElement('img');
 dioImage.src = "dio.jpg";
 let dioAnchor = document.createElement("a");
-dioAnchor.href = "index2.html";
+dioAnchor.href = "dio.html";
 let dioName = document.createElement("p");
 let dioPrice = document.createElement("p");
 dioName.innerHTML = "Dio Brando Statue Resin JOS STUDIO JOJO'S BIZARRE ADVENTURE 1/6";
@@ -68,11 +69,28 @@ dioAnchor.appendChild(dioImage);
 dio.appendChild(dioName);
 dio.appendChild(dioPrice);
 
+let GL = document.createElement("div")
+let GLImage = document.createElement('img');
+GLImage.src = "Gurren.jpg";
+let GLAnchor = document.createElement("a");
+GLAnchor.href = "GL.html";
+let GLName = document.createElement("p");
+let GLPrice = document.createElement("p");
+GLName.innerHTML = "Tengen Toppa Gurren Lagann: Full Drill Version (LIMITED)";
+GLPrice.innerHTML = "$1299";
+GLName.id = "IchigoName";
+GLPrice.id = "IchigoPrice"
+products.appendChild(GL);
+GL.appendChild(GLAnchor);
+GLAnchor.appendChild(GLImage);
+GL.appendChild(GLName);
+GL.appendChild(GLPrice);
+
 let Vasto = document.createElement("div")
 let VastoImage = document.createElement('img');
 VastoImage.src = "Vasto.jpg";
 let VastoAnchor = document.createElement("a");
-VastoAnchor.href = "index2.html";
+VastoAnchor.href = "vasto.html";
 let VastoName = document.createElement("p");
 let VastoPrice = document.createElement("p");
 VastoName.innerHTML = "Ichigo VS Ulqiorra; Elite Fandom Statue";
@@ -103,6 +121,22 @@ DBZ.appendChild(GokuAnchor);
 DBZ.appendChild(GokuName);
 DBZ.appendChild(GokuPrice);
 
+let Ichigo = document.createElement("div")
+let IchigoImage = document.createElement('img');
+IchigoImage.src = "Ichigo.jpg";
+let IchigoAnchor = document.createElement("a");
+IchigoAnchor.href = "index2.html";
+let IchigoName = document.createElement("p");
+let IchigoPrice = document.createElement("p");
+IchigoName.innerHTML = "BLEACH: ICHIGO KUROSAKI 1/6 SCALE PREMIUM STATUE";
+IchigoPrice.innerHTML = "$699";
+IchigoName.id = "IchigoName";
+IchigoPrice.id = "IchigoPrice"
+products.appendChild(Ichigo);
+Ichigo.appendChild(IchigoAnchor);
+IchigoAnchor.appendChild(IchigoImage);
+Ichigo.appendChild(IchigoName);
+Ichigo.appendChild(IchigoPrice);
 
 let Shinra = document.createElement("div")
 let shinraimg = document.createElement("img")
